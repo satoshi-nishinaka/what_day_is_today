7dae4357786ee73e5c38b17a78ccbb66ecf480df
satoshi-nishinaka <satoshie.sp@gmail.com>
2023-07-19 14:03:23 +0900

    Slackに通知できるところまで実装

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/antchfx/htmlquery"
+	"github.com/slack-go/slack"
 )
 
 func main() {
@@ -16,4 +18,18 @@ func main() {
 
 	descriptionElement := htmlquery.FindOne(doc, "//*[@id=\"dateDtl\"]/dd")
 	fmt.Printf("本文: %s\n", htmlquery.InnerText(descriptionElement))
+
+	token := os.Getenv("SLACK_BOT_TOKEN")
+	channel := os.Getenv("SLACK_CHANNEL")
+	if token == "" || channel == "" {
+		log.Fatalln("環境変数が取得できませんでした")
+		return
+	}
+	slackObject := slack.New(token)
+
+	// MsgOptionText() の第二引数に true を設定すると特殊文字をエスケープする
+	_, _, err := slackObject.PostMessage(channel, slack.MsgOptionText(htmlquery.InnerText(titleElement)+"\n\n"+htmlquery.InnerText(descriptionElement), true))
+	if err != nil {
+		panic(err)
+	}
 }
