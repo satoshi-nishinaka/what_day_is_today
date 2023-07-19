22892571193eb6675e9fe0840d034a50094dae78
satoshi-nishinaka <satoshie.sp@gmail.com>
2023-07-19 18:50:21 +0900

    環境変数のチェックは一番最初に行うように変更

--- main.go
+++ main.go
@@ -11,6 +11,13 @@ import (
 
 func main() {
 	log.Println("Start")
+	token := os.Getenv("SLACK_BOT_TOKEN")
+	channel := os.Getenv("SLACK_CHANNEL")
+	if token == "" || channel == "" {
+		log.Fatalln("環境変数が取得できませんでした")
+		return
+	}
+
 	doc, _ := htmlquery.LoadURL("https://kids.yahoo.co.jp/today/")
 
 	titleElement := htmlquery.FindOne(doc, "//*[@id=\"dateDtl\"]/dt/span")
@@ -19,17 +26,14 @@ func main() {
 	descriptionElement := htmlquery.FindOne(doc, "//*[@id=\"dateDtl\"]/dd")
 	fmt.Printf("本文: %s\n", htmlquery.InnerText(descriptionElement))
 
-	token := os.Getenv("SLACK_BOT_TOKEN")
-	channel := os.Getenv("SLACK_CHANNEL")
-	if token == "" || channel == "" {
-		log.Fatalln("環境変数が取得できませんでした")
-		return
-	}
-	slackObject := slack.New(token)
+	message := "✨✨✨ 今日は何の日？ ✨✨✨\n\n**" + htmlquery.InnerText(titleElement) + "**\n\n" + htmlquery.InnerText(descriptionElement)
+	client := slack.New(token)
 
 	// MsgOptionText() の第二引数に true を設定すると特殊文字をエスケープする
-	_, _, err := slackObject.PostMessage(channel, slack.MsgOptionText(htmlquery.InnerText(titleElement)+"\n\n"+htmlquery.InnerText(descriptionElement), true))
+	_, _, err := client.PostMessage(channel, slack.MsgOptionText(message, true))
 	if err != nil {
 		panic(err)
 	}
+
+	log.Println("Finish")
 }
